d864454efa5be595e9630d4191cf98e17841785d
gfloriano11 <gflorianodev@gmail.com>
2025-09-01 21:12:05 -0300

    fix: loop structure to show books data

--- biblioteca/biblioteca.Console/Program.cs
+++ biblioteca/biblioteca.Console/Program.cs
@@ -4,7 +4,7 @@ Console.Clear();
 Console.WriteLine("--- Sistema de Biblioteca: ---");
 livros.Add(new Livro("O Senhor dos Anéis", "JRR Tolkien", "123", 2015, 1247));
 livros.Add(new Livro("Ilíada", "Homero", "437", 2020, 421));
-for (int i = 0; i < livros.Count; i++)
-{
-    livros[i].MostrarDados();
-}
+livros.Add(new Livro("A Faca", "Jo Nesbo", "675", 2020, 456));
+livros.Add(new Livro("Uma História", "Johann Ruth", "1", 2026, 0));
+
+for (int i = 0; i < livros.Count; i++) livros[i].MostrarDados();
