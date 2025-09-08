79873ceca6bac80ad77e97bafe3d8e3e72b08505
gfloriano11 <gflorianodev@gmail.com>
2025-09-08 20:29:35 -0300

    feat: create book controller

---
+++ biblioteca/biblioteca.Console/LivroCRUD.cs
@@ -0,0 +1,12 @@
+public class LivroCRUD
+{
+    private List<Livro> livros;
+    private Livro livro;
+    private int id;
+
+    public LivroCRUD()
+    {
+        this.livros = new List<Livro>();
+        this.livro = new Livro();
+    }
+}
